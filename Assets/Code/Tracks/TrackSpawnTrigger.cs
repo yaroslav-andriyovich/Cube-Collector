26a3b0d88d1ed060685eb1aa5e77f5276c4f406f
Yaroslav Andriyovich <clashhtml@gmail.com>
2025-01-03 05:32:12 +0200

    Track generation cycle

---
+++ Assets/Code/Tracks/TrackSpawnTrigger.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Code.Tracks
+{
+    public class TrackSpawnTrigger : MonoBehaviour
+    {
+        private const string PlayerTag = "Player";
+        
+        private bool _isTriggered;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_isTriggered && IsPlayer(other))
+            {
+                InvokeTrackGenerator();
+                _isTriggered = true;
+            }
+        }
+
+        private bool IsPlayer(Collider targetCollider) => 
+            targetCollider.CompareTag(PlayerTag);
+
+        private void InvokeTrackGenerator()
+        {
+            TrackGenerator trackGenerator = FindObjectOfType<TrackGenerator>();
+
+            trackGenerator.GenerateNext();
+        }
+    }
+}
